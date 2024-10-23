cc9ff27307bb6e613fd2fe5ccfb3e0f4d7b64ee7
Davron <152484311+Davron030901@users.noreply.github.com>
2024-10-23 10:05:23 +0500

    Add files via upload

---
+++ dict_variable.py
@@ -0,0 +1,22 @@
+new=["A","B","C","D","E"]
+for m in new:
+    print(m,end="")
+
+
+l=["geeks",'for','geeks']
+for i in l:
+    print(i)
+
+t=("geeks",'for','geeks')
+for j in t:
+    print(j)
+
+s={1,2,3,4,5,6,}
+for k in s:
+    print(k)
+
+d=dict()
+d["xyz"]=123
+d['abs']=345
+for z in d:
+    print("%s  %d" % (z,d[z]))
